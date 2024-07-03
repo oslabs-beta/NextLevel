449f28834f283779e9cface5a5bd6544f2ceb8e6
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-07-03 15:21:53 -0700

    update index to send to db

--- nextlevelpackage/index.js
+++ nextlevelpackage/index.js
@@ -6,20 +6,32 @@ import { useReportWebVitals } from 'next/web-vitals'
  
 export default function NextWebVitals() {
   useReportWebVitals((metric) => {
-    switch (metric.name) {
-      case 'FCP': {
-        console.log('FCP val:', metric.value);
-        console.log('FCP:', metric);
-        break;
-      }
-      case 'TTFB': {
-        console.log('TTFB val:', metric.value);
-        console.log('TTFB:', metric);
-        break;
-      }
-      default: {
-        break;
-      }
+    const body = JSON.stringify(metric)
+    const url = 'localhost:3000/dashboard/api'
+
+    if(navigator.sendBeacon) {
+      navigator.sendBeacon(url, body)
+    } else {
+      fetch(url, {
+        method: 'POST',
+        body,
+        keepalive: true
+      })
     }
+    // switch (metric.name) {
+    //   case 'FCP': {
+    //     console.log('FCP val:', metric.value);
+        
+    //     break;
+    //   }
+    //   case 'TTFB': {
+    //     console.log('TTFB val:', metric.value);
+    //     console.log('TTFB:', metric);
+    //     break;
+    //   }
+    //   default: {
+    //     break;
+    //   }
+    // }
   })
 }
