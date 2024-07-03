034c522674061fe2cf8cabc7a2fc7d48fc63b5fb
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-07-03 15:17:54 -0700

    using document instead of db to test

---
+++ src/app/dashboard/api/route.js
@@ -0,0 +1,54 @@
+
+// const testData = [
+//     {
+//         metricType: "test",
+//         metricTime: "1",
+//     }
+// ];
+
+// export async function GET () {
+//     return Response.json(testData);
+// }
+
+// export async function POST (request) {
+//     const body = await request.json();
+//     const newData = {
+//       "metricType" : body.name,
+//       "metricTime" : body.time,
+//     }
+//     testData.push(newData);
+//     return new Response(JSON.stringify(newData), {
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       status: 201,
+//     });
+// }
+
+
+
+const fs = require('fs');
+const path = require('path');
+const testDataPath = path.join(process.cwd(), 'data.json');
+
+export async function GET() {
+    const data = JSON.parse(fs.readFileSync(testDataPath, 'utf8'));
+    return Response.json(data);
+}
+
+export async function POST(request) {
+    const body = await request.json();
+    const newData = {
+      "metricType" : body.name,
+      "metricTime" : body.time,
+    };
+    const currentData = JSON.parse(fs.readFileSync(testDataPath, 'utf8'));
+    currentData.testData.push(newData);
+    fs.writeFileSync(testDataPath, JSON.stringify(currentData));
+    return new Response(JSON.stringify(newData), {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      status: 201,
+    });
+}
