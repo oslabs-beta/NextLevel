e4439a868e726de0799cb0794c1412629138f388
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-07-03 11:59:43 -0700

    npm package works

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Dashboard() {
+  return (
+   <main>
+    <h1>Dashboard Page</h1>
+   </main>
+  );
+}
---
+++ src/app/signup/page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Signup() {
+  return (
+   <main>
+    <h1>Signup Page</h1>
+   </main>
+  );
+}
