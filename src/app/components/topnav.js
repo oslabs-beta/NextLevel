9606ad9c5b2da4240fc8e21ab606bf6e4b595c19
kcuomo <kim.cuomo@gmail.com>
2024-07-08 15:40:06 -0500

    update top nav

--- src/app/components/topnav.js
+++ src/app/components/topnav.js
@@ -4,30 +4,29 @@ import Link from 'next/link';
 
 function TopNav({ userLoggedIn, handleLogout }) {
   return (
+    <header className={styles.header}>
     <nav className={styles.nav}>
       <div className={styles.logo}>
         <Link href="/">
-          <a>
-            <img src="/logo.png" alt="Logo" />
-          </a>
+        {/* //add logo */}
+          <img src="/logo.png" alt="Logo" />
         </Link>
       </div>
       <div className={styles.links}>
         <Link href="/">
-          <a>Home</a>
+          Home
         </Link>
-        <Link href="/dashboard">
-          <a>Dashboard</a>
-        </Link>
+        {/* add logout logic?? or move to account page */}
         {userLoggedIn ? (
           <button onClick={handleLogout}>Logout</button>
         ) : (
           <Link href="/login">
-            <a>Login</a>
+            Login
           </Link>
         )}
       </div>
     </nav>
+    </header>
   );
 }
 
