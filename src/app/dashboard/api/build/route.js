097cd0e8db40d58d7f2d27dbc9860d015c5fc027
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-07-10 10:59:10 -0700

    trying with middleware

--- src/app/dashboard/api/build/route.js
+++ src/app/dashboard/api/build/route.js
@@ -6,48 +6,38 @@ const testDataPath = path.join(process.cwd(), 'dataBuild.json');
 
 
 export async function GET(request) {
-  const corsResponse = await corsMiddleware(request);
-  if (corsResponse.status === 204) {
-    return corsResponse;
-  }
-
     try {
-      // await corsMiddleware(req, res);
+      const corsResponse = await corsMiddleware(request);
       console.log('get request made to /dashboard/api/build');
       const data = JSON.parse(await fs.readFile(testDataPath, 'utf8'));
       console.log('build time data:', data);
-      return NextResponse.json(data);
+      return new NextResponse.json(data, { headers: corsResponse.headers }); //does this need the new keyword?
     } catch (error) {
       console.error(error);
-      return new NextResponse(`Internal Server Error: ${error.message}`, { status: 500 });
+      return new NextResponse(`Internal Server Error: ${error.message}`, { status: 500 });  //does this need the new keyword?
     }
 }
 
 export async function POST(request) {
-  const corsResponse = await corsMiddleware(request);
-  if (corsResponse.status === 204) {
-    return corsResponse;
-  }
     try {
-      // await corsMiddleware(req, res);
+      const corsResponse = await corsMiddleware(request); // Apply CORS middleware
       console.log('post request made to /dashboard/api/build');
       const body = await request.json();
       const apiKey = request.headers.get('api-key');
       const newData = {
-        "buildTime": body.buildTime,
+        "buildTime": body.buildTime,   //do the keys need to be strings?
         "apiKey": apiKey,
       };
       const currentData = JSON.parse(await fs.readFile(testDataPath, 'utf8'));
       currentData.testData.push(newData);
       await fs.writeFile(testDataPath, JSON.stringify(currentData));
-      return new NextResponse(JSON.stringify(newData), {
-        headers: {
-          "Content-Type": "application/json"
-        },
+      return new NextResponse(JSON.stringify(newData), {  //does this need the new keyword?
+        headers: corsResponse.headers,
         status: 201,
       });
     } catch (error) {
       console.error(error);
-      return new NextResponse('Internal Server Error', { status: 500 });
+      return new NextResponse('Internal Server Error', { status: 500 });  //does this need the new keyword?
     }
 }
+
