43dcb2caf16ca21aa356aa3b0d9a5c83f3681867
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-07-10 11:13:23 -0700

    testing more cors middleware

--- src/app/dashboard/api/middleware.js
+++ src/app/dashboard/api/middleware.js
@@ -5,6 +5,6 @@ export async function corsMiddleware(request) {
   response.headers.set('Access-Control-Allow-Credentials', 'true');
   response.headers.set('Access-Control-Allow-Origin', '*'); // Replace '*' with your specific origin if needed
   response.headers.set('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT');
-  response.headers.set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, api-key');
+  response.headers.set('Access-Control-Allow-Headers', 'Connection, Postman-Token, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, api-key');
   return response;
 }
